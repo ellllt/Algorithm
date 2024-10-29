723c1b2ad2d9300355ddc02043b4f88c9cbf6d5c
ellllt <aknor1004@gmail.com>
2024-10-29 22:50:06 +0900

    Add 全探索

---
+++ cpp/exhaustive_search.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 全探索基本問題
+// https://atcoder.jp/contests/abc144/tasks/abc144_b
+
+int main() {
+  int N;
+  cin >> N;
+
+  for (int i = 1; i <= 9; i++) {
+    for (int j = 1; j <= 9; j++) {
+      if (i * j == N) {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+ 
+  cout << "No" << endl;
+}
+
